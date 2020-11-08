f3a19b8bdff3949cdddac0134e8fa28080c75b15
Kfir <kfirfer@gmail.com>
2020-11-08 23:35:44 +0200

    wip

---
+++ src/test/java/com/example/javagradleparraleltests/DemoApplicationTests.java
@@ -0,0 +1,86 @@
+package com.example.javagradleparraleltests;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+	@Test
+	public void testSayHelloWorld2() throws Exception {
+		Thread.sleep(2000);
+		System.out.println("test 1");
+		Thread.sleep(2000);
+		System.out.println("test 2");
+	}
+
+
+	@Test
+	public void testSayHelloWorld3() throws Exception {
+		Thread.sleep(2000);
+		System.out.println("test 3");
+		Thread.sleep(2000);
+		System.out.println("test 4");
+	}
+
+	@Test
+	public void testSayHelloWorld4() throws Exception {
+		Thread.sleep(2000);
+		System.out.println("test 5");
+		Thread.sleep(2000);
+		System.out.println("test 6");
+	}
+
+	@Test
+	public void testSayHelloWorld5() throws Exception {
+		Thread.sleep(2000);
+		System.out.println("test 7");
+		Thread.sleep(2000);
+		System.out.println("test 8");
+	}
+
+	@Test
+	public void testSayHelloWorld6() throws Exception {
+		Thread.sleep(2000);
+		System.out.println("test 9");
+		Thread.sleep(2000);
+		System.out.println("test 10");
+	}
+
+	@Test
+	public void testSayHelloWorld7() throws Exception {
+		Thread.sleep(2000);
+		System.out.println("test 11");
+		Thread.sleep(2000);
+		System.out.println("test 12");
+	}
+
+	@Test
+	public void testSayHelloWorld8() throws Exception {
+		Thread.sleep(2000);
+		System.out.println("test 13");
+		Thread.sleep(2000);
+		System.out.println("test 14");
+	}
+
+	@Test
+	public void testSayHelloWorld9() throws Exception {
+		Thread.sleep(2000);
+		System.out.println("test 15");
+		Thread.sleep(2000);
+		System.out.println("test 16");
+	}
+
+	@Test
+	public void testSayHelloWorld10() throws Exception {
+		Thread.sleep(2000);
+		System.out.println("test 17");
+		Thread.sleep(2000);
+		System.out.println("test 18");
+	}
+
+}
